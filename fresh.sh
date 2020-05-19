8de1459f6679ec2c243306c203036f98643d11e8
Robert Krieg <hello@robertkrieg.ch>
2020-05-19 10:47:37 +0200

    Update setup routine

--- fresh.sh
+++ fresh.sh
@@ -2,6 +2,12 @@
 
 echo "Setting up your Mac..."
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until We have finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Add SSH Key to keychain
 ssh-add -K
 
@@ -22,7 +28,15 @@ brew tap homebrew/bundle
 brew bundle
 brew cleanup
 
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
+compaudit | xargs chmod g-w,o-w
+source $HOME/.zshrc
+
 # Install PHP extensions with PECL
+mkdir -p /usr/local/lib/php/pecl # Fixes pecl xdebug install error
 pecl install xdebug imagick
 
 # Install global Composer packages
@@ -32,6 +46,7 @@ pecl install xdebug imagick
 $HOME/.composer/vendor/bin/valet install
 valet tld valet
 valet trust
+valet use php@7.3
 
 cp configs/ext-xdebug.ini /usr/local/etc/php/7.4/conf.d/ext-xdebug.ini
 cp configs/ext-xdebug.ini /usr/local/etc/php/7.3/conf.d/ext-xdebug.ini
@@ -39,13 +54,12 @@ cp configs/ext-xdebug.ini /usr/local/etc/php/7.2/conf.d/ext-xdebug.ini
 
 
 # Install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 nvm install node
 nvm install lts/erbium
 nvm install lts/dubnium
 nvm install lts/carbon
 nvm use stable
-npm -g install @vue/cli netlify-cli
+npm -g install @vue/cli netlify-cli alfred-packagist alfred-npms
 nvm use lts/carbon
 npm -g install gulp
 nvm use stable
@@ -55,15 +69,17 @@ npm config set save true
 
 # Setup mysql
 mysql_install_db
-mysql.server start
+#mysql.server start
+brew services start mariadb
+sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '';"
 
-
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/configs/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/configs/.mackup.cfg $HOME/.mackup.cfg
+
+# Copy gitignore global
+rm -rf $HOME/.gitignore_global
+ln -s $HOME/.dotfiles/configs/.gitignore_global $HOME/.gitignore_global
 
 # Create coding dirs
 mkdir $HOME/workspace
@@ -74,7 +90,10 @@ mkdir $HOME/workspace/freelancing/prototypes
 mkdir $HOME/workspace/freelancing/tools
 
 # Clone Github repositories
-./clone.sh
+./repositiories.sh
+
+# Create project DB's
+./databases.sh
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
